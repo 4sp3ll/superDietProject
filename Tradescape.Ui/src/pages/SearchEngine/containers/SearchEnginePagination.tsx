0c82c5791f7f15b8d17eb6dbbb8f0fb2929ac49b
Tomasz Nowak <angielski.tomek@gmail.com>
2020-12-31 19:09:20 +0100

    pagination - prev,next,first,last func

--- Tradescape.Ui/src/pages/SearchEngine/containers/SearchEnginePagination.tsx
+++ Tradescape.Ui/src/pages/SearchEngine/containers/SearchEnginePagination.tsx
@@ -8,7 +8,6 @@ import {
 } from 'reactstrap';
 import allActions from '../../../actions/index'
 import axios from 'axios'
-import ReactDOM from 'react-dom';
 
 interface State {
     state: object,
@@ -19,7 +18,8 @@ interface State {
     loading: boolean
 }
 
-const SearchEnginePagination = (props: any) => {
+
+const SearchEnginePagination = () => {
     const products = useSelector((state: State) => state.apiSearchEngineReducer.currentState)
     const stringFromRequest = useSelector((state: any) => state.apiSearchEngineReducer.payload)
     const isLoading = useSelector((state: State) => state.apiSearchEngineReducer.loading)
@@ -27,15 +27,15 @@ const SearchEnginePagination = (props: any) => {
     const [selectedState, setSelectedState] = useState(1)
 
 
-    const pagesNumber = products !== null ? (Math.ceil(Number(products.data.count)) / 24) : 0
+    const pagesNumber = products !== null ? Math.ceil((Number(products.data.count)) / 24) : 0
+    console.log(pagesNumber)
     const pagesArray = []
 
     if (products !== null) {
-    for (let i=0; i<=pagesNumber; i++) {
-    pagesArray.push(i)
+      for (let i=0; i<=pagesNumber; i++) {
+        pagesArray.push(i)
+      }
     }
-
-  }
 
   const request = (e: string) => {
     dispatch(allActions.searchEngineBegin())
@@ -66,32 +66,63 @@ const SearchEnginePagination = (props: any) => {
 
     return (
       <Pagination size='lg' aria-label='Page navigation' style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-end'}}>
-      <PaginationItem>
-          <PaginationLink first href="#" />
+      <PaginationItem disabled={selectedState === 1}>
+          <PaginationLink
+          first
+          onClick={() => {
+            request(`${process.env.REACT_APP_API}/cgi/search.pl?action=process&json=true&page_size=24&
+            page=${1}&${stringFromRequest}`);
+            setSelectedState(1)
+          }}
+          />
         </PaginationItem>
-        <PaginationItem>
-          <PaginationLink previous href="#" />
+        <PaginationItem disabled={selectedState === 1}>
+          <PaginationLink
+          previous
+          onClick={() => {
+                    request(`${process.env.REACT_APP_API}/cgi/search.pl?action=process&json=true&page_size=24&
+                    page=${selectedState - 1}&${stringFromRequest}`);
+                    setSelectedState(selectedState - 1)
+          }}
+          />
         </PaginationItem>
         {pagesArray.map((e: any, index, array) => {
           if (array.indexOf(e) < selectedState + 3 && array.indexOf(e) > selectedState - 5) {
             return (
                 <PaginationItem active={e+1 === selectedState} key={index}>
-                  <PaginationLink onClick={() => {
+                  <PaginationLink
+                  onClick={() => {
                     request(`${process.env.REACT_APP_API}/cgi/search.pl?action=process&json=true&page_size=24&page=${e+1}&${stringFromRequest}`);
                     setSelectedState(e+1)
                     }}>
-                    {/* <div ref={props.refProp}/> */}
+
                     {e+1 === selectedState && isLoading ? <Spinner style={{width: '1.5rem', height: '1.5rem'}} animation="border" /> : e+1}
                   </PaginationLink>
                 </PaginationItem>
             )
         }
         })}
-        <PaginationItem>
-          <PaginationLink next href="#" />
+        <PaginationItem disabled={selectedState == pagesNumber}>
+          <PaginationLink
+          next
+          onClick={() => {
+            request(`${process.env.REACT_APP_API}/cgi/search.pl?action=process&json=true&page_size=24&
+            page=${selectedState + 1}&${stringFromRequest}`);
+            setSelectedState(selectedState + 1)
+          }}
+          />
         </PaginationItem>
-        <PaginationItem>
-          <PaginationLink last href="#" />
+        <PaginationItem disabled={selectedState == pagesNumber} >
+          <PaginationLink
+          last
+          onClick={() => {
+            request(`${process.env.REACT_APP_API}/cgi/search.pl?action=process&json=true&page_size=24&
+            page=${pagesNumber}&${stringFromRequest}`);
+            setSelectedState(pagesNumber);
+            console.log(`${process.env.REACT_APP_API}/cgi/search.pl?action=process&json=true&page_size=24&
+            page=${pagesNumber}&${stringFromRequest}`)
+          }}
+          />
         </PaginationItem>
       </Pagination>
     );
