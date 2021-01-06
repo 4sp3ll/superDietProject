67f569f1c8ca22f1ebfed6c2dde77f70523c2ed5
Tomasz Nowak <angielski.tomek@gmail.com>
2021-01-06 18:25:57 +0100

    connected to firebase, login and signup module - first stage

--- Tradescape.Ui/src/firebase/firebase.ts
+++ Tradescape.Ui/src/firebase/firebase.ts
@@ -1,20 +1,16 @@
 import firebase from 'firebase/app'
-import 'firebase/firestore'
 import 'firebase/auth'
 
-var firebaseConfig = {
+const app = firebase.initializeApp({
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
-  };
-  // Initialize Firebase
-//  const fbConfig = firebase.initializeApp(firebaseConfig);
-//   firebase.firestore().settings({ timestampsInSnapshots: true })
+})
 
-// export default firebase
-// export const auth = firebaseConfig.auth()
 
-export default firebaseConfig
+export const auth = app.auth()
+export default app
---
+++ Tradescape.Ui/src/pages/LoginPage/components/Login.tsx
@@ -0,0 +1,59 @@
+import React, { FormEvent, useRef, useState } from 'react'
+import { Form, Button, Card, Alert } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
+import { Link, useHistory } from 'react-router-dom'
+
+interface Props {
+
+}
+
+const Login = (props: Props) => {
+
+    const emailRef = useRef<HTMLInputElement>(null);
+    const passwordRef = useRef<HTMLInputElement>(null);
+    const {login} = useAuth()
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+    const history = useHistory()
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        try {
+            setError('')
+            setLoading(true)
+            await login(emailRef.current?.value, passwordRef.current?.value)
+            history.push('/')
+        } catch {
+            setError(`Error. Something gone wrong.`)
+        }
+        setLoading(false)
+    }
+
+    return (
+        <>
+            <Card>
+                <Card.Body>
+                    <h2 className='text-center mb-4'>Log In</h2>
+                    {error && <Alert variant='danger'>{error}</Alert>}
+                    <Form onSubmit={handleSubmit}>
+                        <Form.Group id='email'>
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type='email' ref={emailRef} required></Form.Control>
+                        </Form.Group>
+                        <Form.Group id='password'>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type='password' ref={passwordRef} required></Form.Control>
+                        </Form.Group>
+                        <Button disabled={loading} className='w-100' type='submit'>Log In</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+            <div className='w-100 text-center mt-2'>
+                Need an account? <Link to='/signup'>Sing Up</Link>
+            </div>
+        </>
+    )
+}
+
+export default Login
