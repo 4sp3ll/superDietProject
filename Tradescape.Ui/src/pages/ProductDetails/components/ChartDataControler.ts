ba14b32071b42d5a695c5ee33462cc4f359fe54d
Tomasz Nowak <angielski.tomek@gmail.com>
2021-01-19 14:11:30 +0100

    ingredients section in product details

--- Tradescape.Ui/src/pages/ProductDetails/components/ChartDataControler.ts
+++ Tradescape.Ui/src/pages/ProductDetails/components/ChartDataControler.ts
@@ -1,47 +1,69 @@
-import {useState} from 'react'
+import {SetStateAction, useState} from 'react'
 
-export const ChartDataControler: any = () => {
+interface Props {
+    carbohydrates: number | string,
+    fat: number | string,
+    proteins: number | string,
+    salt: number | string
+}
 
-    const initialState = {
+interface ObjectConstructor {
+    id: string,
+    label: string
+    value: number,
+    color: string,
+}
+
+export const ChartDataControler = (props: Props) => {
+
+    const {
+        carbohydrates,
+        fat,
+        proteins,
+        salt
+    } = props
+
+    let rest = 100 - Number(carbohydrates) - Number(fat) - Number(proteins) - Number(salt)
+
+    const nutri = {
         fats:  {
             id: "fats",
             label: "fat",
-            value: 80,
+            value: Number(fat),
             color: "hsl(103, 70%, 50%)"
         },
         proteins: {
             id: "proteins",
             label: "proteins",
-            value: 20,
+            value: Number(proteins),
             color: "hsl(48, 70%, 50%)"
         },
         carbs: {
-            id: "carbs",
-            label: "carbs",
-            value: 5,
+            id: "carbohydrates",
+            label: "carbohydrates",
+            value: Number(carbohydrates),
             color: "hsl(48, 70%, 50%)"
         },
         salt: {
             id: "salt",
             label: "salt",
-            value: 2,
+            value: Number(salt),
             color: "hsl(48, 70%, 50%)"
         },
         rest: {
             id: "rest",
             label: "rest",
-            value: 13,
+            value: rest,
             color: "hsl(48, 70%, 50%)"
         }
     }
 
-    const [state, setState] = useState(initialState)
+    const [state, setState]: any = useState(nutri)
 
-    const tabel = []
-    tabel.push(state.fats, state.proteins, state.carbs, state.salt, state.rest)
+    const result: object[] = []
+    result.push(state.fats, state.proteins, state.carbs, state.salt, state.rest)
 
     return (
-        tabel
-
+        result
     )
 }
