838409f560ce8595fbf48168df90fb7b0fd00ea7
Tomasz Nowak <angielski.tomek@gmail.com>
2021-01-22 20:21:23 +0100

    read yourProportion data from firestore

---
+++ Tradescape.Ui/src/firebase/test.ts
@@ -0,0 +1,27 @@
+import React, { ReactElement } from 'react'
+import { useSelector } from 'react-redux'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import app from '../firebase/firebase'
+
+export const yourProportionsToDatabase = (uid: string | undefined, yourProportionData: any) => async () => {
+// export const yourProportionsToDatabase = () => async ({ getFirebase, getFirestore }: any) => {
+// export const yourProportionsToDatabase = () => async (getFirebase: any) => {
+    // const firestore = getFirebase()
+    const db = firebase.firestore(app)
+    // const firestore = getFirebase().firestore()
+
+
+    try {
+        await db
+        .collection('users', )
+        .doc(uid)
+        .set({
+            yourCarbo: yourProportionData.yourCarbo,
+            yourProtein: yourProportionData.yourProtein,
+            yourFat: yourProportionData.yourFat,
+            yourSalt: yourProportionData.yourSalt,
+            yourKcal: yourProportionData.yourKcal
+        })
+    } catch (err) {console.log(err)}
+}
