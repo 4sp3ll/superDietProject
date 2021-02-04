94c1bd7a83367c94d11490b021957659b8af90c9
Tomasz Nowak <angielski.tomek@gmail.com>
2021-02-04 23:32:22 +0100

    acc age custom hook

---
+++ Tradescape.Ui/src/pages/MyMeals/components/ProductElement.tsx
@@ -0,0 +1,110 @@
+import React, { ReactElement, useState } from 'react'
+import ToggleComponent from '../../ui/Toggle'
+import { Button } from 'reactstrap'
+import deleteProduct from '../../../firebase/deleteProduct'
+import deleteDate from '../../../firebase/deleteDate'
+import updateProductQuantity from '../../../firebase/updateProductQuantity'
+
+
+interface Props {
+    id: string,
+    productName: string,
+    thumbnail: string,
+    quantity: number,
+    index: number,
+    carbs: number
+    proteins: number,
+    fat: number,
+    salt: number,
+    kcal: number,
+    stores: Array<string>,
+    date: string,
+    uid: string,
+    dateElement: any
+}
+
+export default function ProductElement(props: Props): ReactElement {
+
+    const {
+        id,
+        productName,
+        thumbnail,
+        quantity,
+        index,
+        carbs,
+        proteins,
+        fat,
+        salt,
+        kcal,
+        stores,
+        date,
+        uid,
+        dateElement
+    } = props
+
+    const [quantityStatus, setQuantityStatus]: any = useState(true)
+    const [inputValue, setInputValue] = useState<number>()
+
+    const updateQuantityValue = () => {
+        setQuantityStatus((prevState: boolean) => !prevState);
+        !quantityStatus && updateProductQuantity(date, id, uid, inputValue)
+    }
+
+    const handleChangeQuantity = (quantity: number, index: number) => {
+        if (quantityStatus) {
+            return `${quantity}g`
+        } else {
+           return (
+           <input
+           value={inputValue}
+           key={index}
+           onChange={(e: any) => setInputValue(e.target.value)}
+           onKeyPress={(e: any) => { if (e.charCode == 13) {
+            updateQuantityValue()
+           }}}
+           />
+           )
+        }
+    }
+
+    return (
+        <>
+        <tr key={id}>
+        <th scope="row"><img src={thumbnail} alt='Product thumbnail' style={{height: '50px'}}/></th>
+        <td>{productName}</td>
+        <td >{handleChangeQuantity(quantity, index)}</td>
+        <td>{carbs}</td>
+        <td>{proteins}</td>
+        <td>{fat}</td>
+        <td>{salt}</td>
+        <td>{kcal}</td>
+        <td>
+            <ToggleComponent
+            content={<ul style={{padding: '0'}}>{stores.map((e: string) => <li style={{ listStyle: 'none'}}>{e}</li>)}</ul>}
+            name='shops'
+            size='sm'
+            />
+        </td>
+        <td style={{width: '20%'}}>
+
+        <Button
+        onClick={() => {
+            updateQuantityValue()
+        }}
+        style={{margin: '1rem'}}
+        size='sm'>
+            {quantityStatus ? 'Change' : 'Save changes'}
+        </Button>
+
+        <Button
+        className='deleteDate'
+        size='sm'
+        onClick={()=> dateElement[0].length < 2 ? deleteDate(date, uid) : deleteProduct(date, id, uid)}>
+            Remove
+        </Button>
+
+        </td>
+        </tr>
+        </>
+    )
+}
--- Tradescape.Ui/src/pages/NavMenu.tsx
+++ Tradescape.Ui/src/pages/NavMenu.tsx
@@ -8,13 +8,14 @@ import ButtonNavStandard from './ui/ButtonNavStandard'
 import './NavMenu.css';
 
 
-export default class NavMenu extends React.PureComponent<{}, { isOpen: boolean }> {
+export default class NavMenu extends React.PureComponent<{accAge: number | undefined}, { isOpen: boolean }> {
   public state = {
     isOpen: false
   };
 
   public render() {
     return (
+
       <header>
         <Navbar className="navbar-expand-sm navbar-toggleable-sm border-bottom mb-3" light style={{ boxShadow: '0 5px 6px -6px #777', backgroundColor: '#FFFFFF', padding: '7px 18px 7px 0' }}>
             <NavbarBrand tag={Link} to="/" className='mr-auto' style={{padding: '0 0 0 5%'}}><img src={Logo}/></NavbarBrand>
@@ -34,9 +35,9 @@ export default class NavMenu extends React.PureComponent<{}, { isOpen: boolean }
                     <NavLink tag={Link} className="text-dark" to="/my-journey" >
                       <BadgeNav
                       title={'My journey'}
-                      amount={'30'}
+                      amount={this.props.accAge}
                       />
-
+                      {console.log(this.props.accAge)}
                     </NavLink>
                   </NavItem>
                   <NavItem style={{ margin: "4px 4px 0px 4px", padding: "4px 0 4px 0" }}>
