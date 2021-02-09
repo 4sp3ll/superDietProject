0bdb7aef1a439324ea5e8a0683636810f8d3c755
Tomasz Nowak <angielski.tomek@gmail.com>
2021-02-09 19:16:57 +0100

    react-bootstrap part 1

--- Tradescape.Ui/src/pages/SearchEngine/components/TableResult.tsx
+++ Tradescape.Ui/src/pages/SearchEngine/components/TableResult.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import styled from "styled-components";
+import styled from 'styled-components'
 
 const Th = styled.th`{
     white-space: nowrap;
--- Tradescape.Ui/src/pages/SearchEngine/containers/AddProduct.tsx
+++ Tradescape.Ui/src/pages/SearchEngine/containers/AddProduct.tsx
@@ -1,6 +1,7 @@
 import React, { ReactElement, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { Modal, ModalHeader, ModalBody, ModalFooter, Col, Input, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap'
+// import { Modal, ModalHeader, ModalBody, ModalFooter, Col, Input, InputGroup, InputGroupAddon, InputGroupText } from 'reactstrap'
+import { Modal, Col, InputGroup, FormControl } from 'react-bootstrap'
 import { Button } from 'react-bootstrap'
 import allActions from '../../../actions/index'
 import addProductToDatabase from '../../../firebase/addProductToDatabase'
@@ -9,9 +10,7 @@ export default function AddProduct({productNumber}: any): ReactElement {
 
     const [modal, setModal] = useState(false);
     const toggle = () => setModal(!modal);
-
     const [quantity, setQuantity] = useState<number>()
-
     const dispatch = useDispatch()
 
     const {
@@ -29,29 +28,29 @@ export default function AddProduct({productNumber}: any): ReactElement {
     return (
         <div>
             <Button variant='orange-light' onClick={toggle} className='shadow-none'>+</Button>
-            <Modal isOpen={modal} toggle={toggle} className="product-details" size="md" centered={true} scrollable={true}>
-                <ModalHeader toggle={toggle}>
-                    <img src={photoThumb}/> <p style={{display: 'inline-block'}}>{productName}</p>
-                </ModalHeader>
-                <ModalBody>
-                    <Col  sm={{ size: 8, offset: 2 }}>
+            <Modal show={modal} onHide={() => toggle()} toggle={toggle} className="product-details" centered={true} scrollable={true}>
+                <Modal.Header>
+                    {/* <img src={photoThumb}/> <p style={{display: 'inline-block'}}>{productName}</p> */}
+                    <img src={photoThumb}/> <h4 style={{textAlign: 'left'}}>{productName}</h4>
+                </Modal.Header>
+                <Modal.Body>
+                    <Col  sm={{ span: 8, offset: 2 }}>
                         <InputGroup>
-                            <InputGroupAddon addonType="append">
-                                <InputGroupText>Quantity:</InputGroupText>
-                            </InputGroupAddon>
-                            <Input
+                            <InputGroup.Append>
+                                <InputGroup.Text>Quantity:</InputGroup.Text>
+                            </InputGroup.Append>
+                            <FormControl
                             placeholder="grams"
                             type="number"
                             min="1"
                             max="10000000000"
-                            oninput="this.value = Math.abs(this.value)"
                             onChange={(e: any) => setQuantity(parseInt(e.target.value))}
                             value={quantity}
                             />
                         </InputGroup>
                     </Col>
-                </ModalBody>
-                <ModalFooter>
+                </Modal.Body>
+                <Modal.Footer>
                     <Button
                     variant='orange'
                     className='shadow-none'
@@ -61,7 +60,7 @@ export default function AddProduct({productNumber}: any): ReactElement {
                         Add product
                     </Button>{' '}
                     <Button variant='white' className='shadow-none' onClick={toggle}>Cancel</Button>
-                </ModalFooter>
+                </Modal.Footer>
             </Modal>
         </div>
     )
